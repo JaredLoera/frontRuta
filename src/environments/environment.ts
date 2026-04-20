238d27a427466c0adc1e91392892b8ad14a3724c
jared <salazarloerajared@gmail.com>
2026-04-20 01:21:21 -0600

    <FIX>: url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:3000/',
+       apiUrl: 'https://hinojosadevs.online/api/',
     storageNames: {
         token: 'auth_token',
         user: 'auth_user',
